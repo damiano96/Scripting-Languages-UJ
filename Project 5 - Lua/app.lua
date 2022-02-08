03926a77d7d91702f50a904bfa55b0c9bbdaebae
Damian <36972355+damiano96@users.noreply.github.com>
2022-02-08 23:46:45 +0100

    Add files via upload

---
+++ Project 5 - Lua/app.lua
@@ -0,0 +1,103 @@
+local lapis = require "lapis"
+local app_helpers = require("lapis.application")
+local Model = require("lapis.db.model").Model
+local preload = require("lapis.db.model").preload
+local validate = require("lapis.validate")
+local capture_errors = app_helpers.capture_errors
+
+local app = lapis.Application()
+
+app:get("/products/:product_id", function(self)
+  local productID = self.params.product_id
+  local Products = Model:extend("products")
+  local product = Products:find(productID)
+
+  if product then
+    return { json = product, status = 200 }
+  else
+    return { json = { code = "404", message = "Brak produktu o tym id" }, status = 404 }
+  end
+end)
+
+app:post("/products/add", capture_errors({
+  on_error = function(self)
+    return { json = { code = "400", message = "Nieprawidlowy obiekt produktu. Popraw!" }, status = 400 }
+  end, 
+  function(self)
+    validate.assert_valid(self.params, {
+      { "name", exists = true},
+      { "price", exists = true},
+      { "year", exists = true},
+      { "category_name", exists = true }
+    })
+
+    local Categories = Model:extend("categories")
+    local category = Categories:find({name = self.params.category_name})
+
+    if category then
+      local Products = Model:extend("products")
+      local product = Products:create({
+        name = self.params.name,
+        price = self.params.price,
+        year = self.params.year,
+        category_id = category.id
+      })
+
+      return {
+        json = product,
+        status = 201
+      }
+    end
+
+    return { json = { code = "400", message = "Cos poszlo nie tak. Sprawdz dane!" }, status = 400 }
+  end
+}))
+
+app:delete("/products/delete", function(self)
+  local product_id = self.params.id_product
+  local Products = Model:extend("products")
+
+  local product = Products:find(product_id)
+
+  if product then
+    product:delete()
+    return {
+      json = "Pomyslnie usunieto produkt"
+      status = 200
+    }
+  end
+
+  return { json = { code = "400", message = "Cos poszlo nie tak. Sprawdz dane!" }, status = 400 }
+  
+end)
+
+
+app:get("/products/all", function(self)
+  local Products = Model:extend("products")
+  local products = Products:select("INNER JOIN categories WHERE categories.id = products.category_id", 
+    { fields = "products.id, products.name, products.price, products.year, categories.name as category" }
+  )
+
+  return { json = products, status = 200 }
+end)
+
+
+app:get("/categories", function(self)
+  local model = Model:extend("categories")
+  local categories = model:select()
+
+  return { json = categories, status = 200 }
+end)
+
+
+app:get("/categories/:category", function(self)
+  local categoryName = self.params.category
+  local Products = Model:extend("products")
+  local products = Products:select("INNER JOIN categories WHERE categories.id = products.category_id AND categories.name = ?", categoryName,
+    { fields = "products.id, products.name, products.price, products.year, categories.name as category" }
+  )
+
+  return { json = products, status = 200 }
+
+end)
+return app
